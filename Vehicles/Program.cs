204ab1f3f49353805f382739971a1bbd8f80dc4b
gabrielfontineli <gabriel0fontineli@gmail.com>
2021-07-12 17:30:53 -0300

    vehicle code

---
+++ Vehicles/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo moto = new Veiculo();
+            moto.conduzir();
+            Aviao boeing = new Aviao();
+            boeing.conduzir();
+            Carro l200 = new Carro();
+            l200.conduzir();
+        }
+    }
+    class Aviao : Veiculo
+    {
+        public virtual void conduzir()
+        {
+            Console.WriteLine("Estou pilotando!");
+        }
+    }
+    class Veiculo
+    {
+        private void ligarMotor()
+        {
+            Console.WriteLine("Motor ligado!!");
+        }
+        private void desligarMotor()
+        {
+            Console.WriteLine("Motor desligado!!");
+        }
+        public void conduzir()
+        {
+            Console.WriteLine("Estou conduzindo!");
+        }
+
+    }
+    class Carro : Veiculo
+    {
+        public virtual void conduzir()
+        {
+            Console.WriteLine("Estou dirigindo");
+        }
+    }
+}
