a4c6ecd2c7540e6392e250e2ac68542df78dde22
gabrielfontineli <gabriel0fontineli@gmail.com>
2021-07-14 13:51:28 -0300

    prompt data instead of giving them in constructor

--- LastProject/Program.cs
+++ LastProject/Program.cs
@@ -6,16 +6,33 @@ namespace LastProject
     {
         static void Main(string[] args)
         {
+
+            DateTime DayOfBirth ;
             
-            DateTime DayOfBirth = DateTime.Parse("02/09/2004");
-            Person Me = new Person("Gabriel Fontineli", "Dantas",DayOfBirth , 1.80);
+            string name,surname;
+            double height;
+            
+            Console.WriteLine("type your name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("type your surname");
+            surname = Console.ReadLine();
+            
+            Console.WriteLine("type your height in meters");
+            height = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("type your birth day");
+            DayOfBirth = DateTime.Parse(Console.ReadLine());
+            
+
+            Person OtherPerson = new Person(name,surname, DayOfBirth,height);
             Console.WriteLine("-----------------------------------------------------------");
-            Me.ShowData();
-            Console.WriteLine("-----------------------------------------------------------");            
-            Me.ShowAge();
-            Console.WriteLine("-----------------------------------------------------------");            
-            Me.ShowNameLikeUS();
-            Console.WriteLine("-----------------------------------------------------------");            
+            OtherPerson.ShowData();
+            Console.WriteLine("-----------------------------------------------------------");
+            OtherPerson.ShowAge();
+            Console.WriteLine("-----------------------------------------------------------");
+            OtherPerson.ShowNameLikeUS();
+            Console.WriteLine("-----------------------------------------------------------");
 
         }
     }
